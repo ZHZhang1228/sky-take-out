cac722d90bb5f2562555956a9d21fd967a6e4969
zzh <522586126@qq.com>
2023-08-31 11:28:44 +0800

    增加用户下单提醒功能

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -1,9 +1,32 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.entity.Orders;
+import io.swagger.models.auth.In;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Mapper
 public interface OrderMapper {
     void insert(Orders orders);
+
+    @Select("select * from orders where number = #{orderNumber}")
+    Orders getByNumber(String orderNumber);
+
+    void update(Orders orders);
+
+    Page<Orders> pageQuery(OrdersPageQueryDTO ordersPageQueryDTO);
+    @Select("select * from orders where id=#{id}")
+    Orders getById(Long id);
+
+    @Select("select count(id) from orders where status = #{status}")
+    Integer countStatus(Integer toBeConfirmed);
+
+    //@Select("select * from orders where status = ? and order_time < #{orderTime}")
+    @Select("select * from orders where status = #{status} and order_time< #{orderTime}")
+    List<Orders> getByStatusandOrderTime(Integer status, LocalDateTime orderTime);
 }
